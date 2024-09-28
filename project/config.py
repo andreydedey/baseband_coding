b8ff235def9cbdc7cf6702494de93b44786d785b
Andrey Oliveira <dedey.cardoso@gmail.com>
2024-09-28 20:15:47 -0300

    images in the correct place

--- project/config.py
+++ project/config.py
@@ -3,6 +3,6 @@ SEQUENCE_1 = '1000000001010011'
 SEQUENCE_2 = '1110100101000010'
 
 # Diretórios para salvar gráficos
-OUTPUT_DIR_HDB3 = "img/hdb3_plots"
-OUTPUT_DIR_NRZI = "img/nrzi_plots"
-OUTPUT_DIR_MANCHESTER_DIFERENCIAL = "img/manchester_differential"
+OUTPUT_DIR_HDB3 = "project/img/hdb3_plots"
+OUTPUT_DIR_NRZI = "project/img/nrzi_plots"
+OUTPUT_DIR_MANCHESTER_DIFERENCIAL = "project/img/manchester_differential"
